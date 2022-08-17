ff51fe062ec3529225a35270ccf0b3d4e28ada3e
Tarcisio Philips <tarcisio.philips@escoladnc.com.br>
2022-08-17 07:55:42 -0300

    first config - Context component

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,15 @@
-import React from "react";
+import React, {useContext} from "react";
+
+import { Context } from "../context";
+
+import { useRouter } from "next/router";
+import  axios  from "axios";
+
 
 export default function Auth() {
-  return <div className="background">auth</div>;
+  return <div className="background">
+    <div className="auth-container">
+      <form className="auth-form" onSubmit={ e => e.preventDefault()} ></form>
+    </div>
+  </div>;
 }
